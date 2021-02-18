0c2439e58b0a45e8e9d4b961140cee668ac13a96
Jackson Gill <jacksongill@Jacksons-MacBook-Pro-3.local>
2021-02-17 22:15:51 -0600

    server index.html to server

--- news-app/app.js
+++ news-app/app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const newsRouter = require('./controllers/newsRouter');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 app.use(newsRouter);
 
-app.get('/', (_request, response) => {
-  response.sendFile(__dirname, '/build/index.html');
+app.get('*', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 module.exports = app;
